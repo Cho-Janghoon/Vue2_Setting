b244a5397dd1d6739dae1e8d435796c0375c46b5
Answer-John <john@ianswer.co.kr>
2022-10-20 16:07:09 +0900

    :Update: last commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,23 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  env: {
-    node: true
-  },
+    env: {
+        node: true
+    },
 
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+    extends: ["plugin:vue/essential", "@vue/standard", "prettier"],
 
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
+    parserOptions: {
+        parser: "@typescript-eslint/parser"
+    },
 
-  rules: {
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
+    rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "prettier/prettier": "error"
+    },
 
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript',
-    "prettier"
-  ]
-}
+    extends: ["plugin:vue/essential", "@vue/standard", "@vue/typescript", "prettier"]
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,8 @@
-<style scoped>
-.red {
-    color: red
-}
-</style>
 <template>
     <div>
-        <router-link to="/about" class="red">{{title}}</router-link>
-        <input type="text" v-model="input1"/>
-        <h1>{{input1}}</h1>
-
-        <select class="form-control" v-model="region" @change="changeRegion">
-            <Option :key="idx" :value="el.v" v-for="(el, idx) in options">{{el.t}}</Option>
-        </select>
-
-        <table class="table table-bordered">
-            <tr :key="idx" v-for="(el, idx) in options">
-                <td>{{el.v}}</td>
-                <td>{{el.t}}</td>
-            </tr>
-        </table>
+        <h4>{{ title }}</h4>
+        <input type="password" v-model="inputValue" @input="inputWatch" />
+        <div>미리보기: {{ inputValue }}</div>
     </div>
 </template>
 <script>
@@ -26,48 +10,24 @@ export default {
     data() {
         return {
             title: "Hello World!",
-            input1: "",
-            options: [
-                {v: "S", t: "Seoul"},
-                {v: "J", t: "Jeju"},
-                {v: "B", t: "Busan"}
-            ],
-            region: ""
-        }
+            inputValue: ""
+        };
     },
-    watch: {
-        input1() {
-            console.log("watch value:", this.input1)
-        }
-    },
+    watch: {},
     methods: {
-        changeRegion() {
-            alert(this.region)
+        inputWatch: function (e) {
+            console.log(e.target.value);
         }
     },
-    beforeCreate() {
-        console.log("beforeCreate")
-    },
-    created() {
-        console.log("created")
-    },
-    beforeMount() {
-        console.log("beforeMount")
-    },
-    mounted() {
-        console.log("mounted")
-    },
-    beforeUpdate() {
-        console.log("beforeUpdate")
-    },
-    updated() {
-        console.log("updated")
-    },
-    beforeDestroy() {
-        console.log("beforeDestroy")
-    },
-    destroyed() {
-        console.log("destroyed")
-    }
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {}
 };
 </script>
+
+<style scoped></style>
